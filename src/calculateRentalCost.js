6245ccf29133875df3db00bedb76afb92b1934e3
RafaelGratton <grataorafael17@gmail.com>
2025-12-17 10:17:47 -0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,15 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let totalCost = days * 40;
+
+  if (days >= 7) {
+    totalCost -= 50; // Desconto de $50 para 7 ou mais dias
+  } else if (days >= 3) {
+    totalCost -= 20; // Desconto de $20 para 3 ou mais dias
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
